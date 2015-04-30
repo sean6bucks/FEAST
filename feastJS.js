5ffae86b2d24bc20123b19dba53385ae4a0b5e34
Sean6bucks <sean6bucks@gmail.com>
2015-04-29 23:42:18 -0400

    FInally got the infinite loop to work with the help of TweenMax JS library

--- feastJS.js
+++ feastJS.js
@@ -1,8 +1,49 @@
 $(document).ready(function(){
 
+window.onload = function (){
+
+
+
+var scroller = function(){
+   var thumb1 = document.getElementsByClassName("homeThumb1");
+   TweenMax.from(thumb1, 8, {left:"120%", repeat:-1, ease:Linear.easeNone});
+   
+   var thumb2 = document.getElementsByClassName("homeThumb2");
+   TweenMax.from(thumb2, 8, {left:"120%", repeat:-1, delay: 2, ease:Linear.easeNone});
+
+   var thumb3 = document.getElementsByClassName("homeThumb3");
+   TweenMax.from(thumb3, 8, {left:"120%", repeat:-1, delay: 4, ease:Linear.easeNone});
+   
+   var thumb4 = document.getElementsByClassName("homeThumb4");
+   TweenMax.from(thumb4, 8, {left:"120%", repeat:-1, delay: 6, ease:Linear.easeNone});
+   
+   var thumb5 = document.getElementsByClassName("homeThumb5");
+   TweenMax.from(thumb5, 2, {left:"0%", ease:Linear.easeNone});
+   
+   var thumb6 = document.getElementsByClassName("homeThumb6");
+   TweenMax.from(thumb6, 4, {left:"40%", ease:Linear.easeNone});
+   
+   var thumb7 = document.getElementsByClassName("homeThumb7");
+   TweenMax.from(thumb7, 6, {left:"80%", ease:Linear.easeNone});
+
+	};
+
+scroller();
+
+$('.largeText').mouseenter(function(){
+	scroller.restart();
+});
+
+
+}
+
+/* SECOND VERSION, inifinite loop by glitchy animation causes delay and disaster
+var firstImg = $('.scrollBox img:first-child')
+var secondImg = $('.scrollBox img:nth-child(2)')
+var thirdImg = $('.scrollBox img:nth-child(3)')
+var lastImg = $('.scrollBox img:nth-child(4)')
 
 var thumb1 = function(){
-	var firstImg = $('.scrollBox img:first-child')
 	firstImg.animate({"left":"-40%"}, 2000, "linear", function(){
 		firstImg.css('left','120%');
 		firstImg.animate({'left':'0%'}, 6000, "linear");
@@ -10,7 +51,6 @@ var thumb1 = function(){
 }
 
 var thumb2 = function(){
-	var secondImg = $('.scrollBox img:nth-child(2)')
 	secondImg.animate({"left":"-40%"}, 4000, "linear", function(){
 		secondImg.css('left','120%');
 		secondImg.animate({'left':'40%'}, 4000, "linear");
@@ -18,7 +58,6 @@ var thumb2 = function(){
 }
 
 var thumb3 = function(){
-	var thirdImg = $('.scrollBox img:nth-child(3)')
 	thirdImg.animate({"left":"-40%"}, 6000, "linear", function(){
 		thirdImg.css('left','120%');
 		thirdImg.animate({'left':'80%'}, 2000, "linear");
@@ -26,22 +65,21 @@ var thumb3 = function(){
 }
 
 var thumb4 = function(){
-	var lastImg = $('.scrollBox img:nth-child(4)')
 	lastImg.animate({"left":"-40%"}, 8000, "linear", function(){
 		lastImg.css('left','120%');
 		// lastImg.animate({'left':'120%'}, 0, "linear");
 	});
 }
 
-
 setInterval(function(){
-		thumb1();
-		thumb2();
-		thumb3();
-		thumb4();
+	thumb1();
+	thumb2();
+	thumb3();
+	thumb4();
 }, 8000);
+*/
 
-/*
+/* ORIGINAL VERSION with TWICE scroll
 var galleryScroll = function(){
 	
 
